7464b40dc4a67a715be637a94c46ba407de6b786
akshaydig <you@example.com>
2021-12-22 10:44:54 +0530

    Added: Peak And Pop Method

--- src/com/bridgelabz/datastructures/Stack.java
+++ src/com/bridgelabz/datastructures/Stack.java
@@ -3,15 +3,28 @@ package com.bridgelabz.datastructures;
 import java.util.LinkedList;
 
 public class Stack {
-	LinkedList<Integer> linkedList = new LinkedList<>();
+	LinkedList<Integer> list = new LinkedList<>();
 
-	public void insert(int data) {
-		linkedList.addFirst(data);
+	public void insert(int data) { // inserting value
+		list.addFirst(data);
 	}
 
-	public void display() {
-		for (Integer integer : linkedList) {
-			System.out.println(" " + integer);
+	public void display() { // Displaying inserted value
+		System.out.println("Elements in Link list");
+		for (Integer integer : list) {
+			System.out.println(integer);
+		}
+	}
+
+	public void peak() {
+		System.out.println("Peak Element :" + list.peek()); // peak no
+	}
+
+	public void pop() { // pop no
+		if (list.size() != 0) {
+			list.pop();
+		} else {
+			System.out.println("Stack is Empty");
 		}
 	}
 }
