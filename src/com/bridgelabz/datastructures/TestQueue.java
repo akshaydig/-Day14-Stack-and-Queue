1d8aa4ae78234fa373afd4b45ecb1816b0ddaa08
akshaydig <you@example.com>
2021-12-22 10:58:50 +0530

    Added: Dequeue Creation

--- src/com/bridgelabz/datastructures/TestQueue.java
+++ src/com/bridgelabz/datastructures/TestQueue.java
@@ -3,10 +3,16 @@ package com.bridgelabz.datastructures;
 public class TestQueue {
 	public static void main(String[] args) {
 		Queue queue = new Queue();
-
 		queue.insert(56);
 		queue.insert(30);
 		queue.insert(70);
 		queue.display();
+		queue.peak();
+		queue.pop();
+		queue.display();
+		queue.pop();
+		queue.display();
+		queue.pop();
+		queue.pop();
 	}
 }
